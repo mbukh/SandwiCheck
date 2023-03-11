31d11ecfd186417fd3e95fcb350ffc9616f64a8a
mbukhman <mbukhman@gmail.com>
2023-03-12 00:01:43 +0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import github from "../assets/images/icons/github.svg";
 
 const Footer = () => {
-    return <></>;
+    return (
+        <footer className="footer fixed z-10 bottom-0 w-full bg-cyan2 text-white">
+            <div className="flex items-center h-full px-5 md:px-12 xl:px-20">
+                <nav className="footer__nav">
+                    <ul className="flex text-xxs md:text-xs text-center uppercase text-shadow-3">
+                        <li className="mr-4 md:mr-8">
+                            <Link
+                                className="hover:opacity-80"
+                                to="#modal-terms"
+                                data-modal=""
+                            >
+                                Terms
+                            </Link>
+                        </li>
+                        <li className="mr-4 md:mr-8">
+                            <Link className="hover:opacity-80" to="#modal-privacy">
+                                Privacy
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div className="footer__triangle absolute bottom-0 right-0 pointer-events-none">
+                <Link
+                    className="pointer-events-auto"
+                    to="https://www.lidl.ie/"
+                    target="_blank"
+                >
+                    <img
+                        className="footer__logo block md:w-16 absolute bottom-0 right-0 mb-4 md:mb-5 mr-5 md:mr-12 xl:mr-20"
+                        src={github}
+                        alt="Github logo"
+                        width="50"
+                        height="50"
+                    />
+                </Link>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
