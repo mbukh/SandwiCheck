ff253315b76ac55cf12cdb1ec4943be59ae0bebc
mbukhman <mbukhman@gmail.com>
2023-04-02 21:06:08 +0300

    port 5000

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Server running"));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
