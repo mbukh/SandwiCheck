c2a1d8f863aa2bc1fe19c64c3895b4e8019ce4f3
mbukhman <mbukhman@gmail.com>
2023-04-02 14:08:21 +0200

    export Firebase database

--- src/constants/debug.js
+++ src/constants/debug.js
@@ -1,4 +1,4 @@
 export const debug = true;
 export const initialize = false;
 export const rebuildUsers = false; // !!! don't use it. it will remove all users. it stops initialization of other data.
-export const exportJSON = true;
+export const exportJSON = false;
--- src/services/Firestore/exportFirestoreToJSON.js
+++ src/services/Firestore/exportFirestoreToJSON.js
@@ -5,18 +5,31 @@ const getAllDatabaseAsJSON = async () => {
     try {
         const usersSnapshot = await getDocs(collection(db, "users"));
         const data = {};
+        const users = {};
+        const sandwiches = {};
         for (const userDoc of usersSnapshot.docs) {
             data[userDoc.id] = userDoc.data();
+            users[userDoc.id] = userDoc.data();
             const sandwichesSnapshot = await getDocs(
                 collection(doc(db, "users", userDoc.id), "sandwiches")
             );
             const sandwichesData = {};
             for (const sandwichDoc of sandwichesSnapshot.docs) {
                 sandwichesData[sandwichDoc.id] = sandwichDoc.data();
+                sandwiches[sandwichDoc.id] = sandwichDoc.data();
+                sandwiches[sandwichDoc.id].userId = userDoc.id;
             }
             data[userDoc.id]["sandwiches"] = sandwichesData;
         }
+        console.log("======== ALL DATA ========");
         console.log(JSON.stringify(data));
+        console.log("======== ALL DATA ========");
+        console.log("======== USERS DATA ========");
+        console.log(JSON.stringify(users));
+        console.log("======== USERS DATA ========");
+        console.log("======== SANDWICHES DATA ========");
+        console.log(JSON.stringify(sandwiches));
+        console.log("======== SANDWICHES DATA ========");
         console.log("Data exported successfully!");
     } catch (error) {
         console.error(error);
