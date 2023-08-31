fdee2272b819fd7d595203e304162d4fbffefa37
mbukhman <mbukhman@gmail.com>
2023-08-31 08:29:34 +0400

    fix test with subfolder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,93 +18,90 @@ import SandwichGallery from "./components/Sandwich/SandwichGallery";
 import SandwichModal from "./components/Sandwich/SandwichModal";
 import SignupModal from "./components/Signup/SignupModal";
 
-const router = createHashRouter(
-  [
-    {
-      path: "/sandwich/:sandwichId",
-      element: <SandwichModal closeLink="/latest" />,
-    },
-    {
-      path: "/login",
-      element: <LoginModal />,
-      children: [
-        {
-          path: "parent/:parentId",
-          element: <LoginModal />,
-        },
-      ],
-    },
-    {
-      path: "/signup",
-      element: <SignupModal />,
-      children: [
-        {
-          path: "parent/:parentId",
-          element: <SignupModal />,
-        },
-      ],
-    },
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <SandwichGallery galleryType="latest" />,
-        },
-        {
-          path: "/best",
-          element: <SandwichGallery galleryType="best" />,
-        },
-        {
-          path: "/latest/:sandwichId",
-          element: (
-            <SandwichGallery galleryType="latest">
-              <SandwichModal />
-            </SandwichGallery>
-          ),
-        },
-        {
-          path: "/create",
-          element: (
-            <SandwichContextProvider>
-              <SandwichBuilder />
-            </SandwichContextProvider>
-          ),
-        },
-        {
-          path: "/menu",
-          element: <SandwichGallery galleryType="personal" />,
-        },
-        {
-          path: "/menu/sandwich/:sandwichId",
-          element: <SandwichModal />,
-        },
-        {
-          path: "/family",
-          element: <Family />,
-        },
-        {
-          path: "/family/:childId",
-          element: <SandwichGallery />,
-        },
-        {
-          path: "/family/:childId/sandwich/:sandwichId",
-          element: <SandwichModal />,
-        },
-        {
-          path: "/cart",
-          element: <Cart />,
-        },
-      ],
-    },
-    {
-      path: "*",
-      element: <Error404 />,
-    },
-  ],
-  { basename: process.env.REACT_APP_PATH }
-);
+const router = createHashRouter([
+  {
+    path: "/sandwich/:sandwichId",
+    element: <SandwichModal closeLink="/latest" />,
+  },
+  {
+    path: "/login",
+    element: <LoginModal />,
+    children: [
+      {
+        path: "parent/:parentId",
+        element: <LoginModal />,
+      },
+    ],
+  },
+  {
+    path: "/signup",
+    element: <SignupModal />,
+    children: [
+      {
+        path: "parent/:parentId",
+        element: <SignupModal />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <SandwichGallery galleryType="latest" />,
+      },
+      {
+        path: "/best",
+        element: <SandwichGallery galleryType="best" />,
+      },
+      {
+        path: "/latest/:sandwichId",
+        element: (
+          <SandwichGallery galleryType="latest">
+            <SandwichModal />
+          </SandwichGallery>
+        ),
+      },
+      {
+        path: "/create",
+        element: (
+          <SandwichContextProvider>
+            <SandwichBuilder />
+          </SandwichContextProvider>
+        ),
+      },
+      {
+        path: "/menu",
+        element: <SandwichGallery galleryType="personal" />,
+      },
+      {
+        path: "/menu/sandwich/:sandwichId",
+        element: <SandwichModal />,
+      },
+      {
+        path: "/family",
+        element: <Family />,
+      },
+      {
+        path: "/family/:childId",
+        element: <SandwichGallery />,
+      },
+      {
+        path: "/family/:childId/sandwich/:sandwichId",
+        element: <SandwichModal />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Error404 />,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
